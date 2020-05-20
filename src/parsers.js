840a5bbcc6bafa8c8ae48be1ab02a52a5aa65b27
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-05-20 22:41:34 +0300

    add parse ini

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parser = (pathToFile) => {
   if (path.extname(pathToFile) === '.json') {
     return JSON.parse(fs.readFileSync(pathToFile));
   }
-  return yaml.safeLoad(fs.readFileSync(pathToFile));
+  if (path.extname(pathToFile) === '.yml') {
+    return yaml.safeLoad(fs.readFileSync(pathToFile));
+  }
+  return ini.parse(fs.readFileSync(pathToFile, 'utf-8'));
 };
 export default parser;
