c0be92d8b17d3d24d3728a61414bd6859f91de15
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-05-20 23:33:04 +0300

    add ini test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,20 @@
 import path from 'path';
-import gendiff from '../src/genDiff';
+import genDiff from '../src/genDiff';
 
 const result = '{\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 test('gendiffJSON', () => {
   const firstFileJson = getFixturePath('before.json');
   const secondFileJson = getFixturePath('after.json');
-  expect(gendiff(firstFileJson, secondFileJson)).toEqual(result);
+  expect(genDiff(firstFileJson, secondFileJson)).toEqual(result);
 });
 test('gendiffYML', () => {
   const firstFileYml = getFixturePath('before.yml');
   const secondFileYml = getFixturePath('after.yml');
-  expect(gendiff(firstFileYml, secondFileYml)).toEqual(result);
+  expect(genDiff(firstFileYml, secondFileYml)).toEqual(result);
 });
 test('gendiffINI', () => {
   const firstFileINI = getFixturePath('before.ini');
   const secondFileINI = getFixturePath('after.ini');
-  expect(gendiff(firstFileINI, secondFileINI)).toEqual(result);
+  expect(genDiff(firstFileINI, secondFileINI)).toEqual(result);
 });
