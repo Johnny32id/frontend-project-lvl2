2ee5268f1556c1eccbb59c016fec0fd8b29768cd
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-05-20 20:16:59 +0300

    add yml test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,15 @@
 import path from 'path';
-import genDiff from '../src/genDiff';
+import gendiff from '../src/genDiff';
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const result = '{\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
-test('gendiff', () => {
-  const pathToFirstFile = getFixturePath('before.json');
-  const pathToSecondFile = getFixturePath('after.json');
-  expect(genDiff(pathToFirstFile, pathToSecondFile)).toEqual(result);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+test('gendiffJSON', () => {
+  const firstFileJson = getFixturePath('before.json');
+  const secondFileJson = getFixturePath('after.json');
+  expect(gendiff(firstFileJson, secondFileJson)).toEqual(result);
+});
+test('gendiffYML', () => {
+  const firstFileYml = getFixturePath('before.yml');
+  const secondFileYml = getFixturePath('after.yml');
+  expect(gendiff(firstFileYml, secondFileYml)).toEqual(result);
 });
