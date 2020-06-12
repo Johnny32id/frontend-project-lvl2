618ec521e10050bc3e8e55661171459507b7981a
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-06-12 16:36:48 +0300

    add formater

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,15 +2,14 @@
 
 import program from 'commander';
 import genDiff from '../genDiff';
-import stylish from '../stylish';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format [type] defaulting to stylish', stylish)
+  .option('-f, --format [type]', 'output format [type] defaulting to stylish', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+  .action((filepath1, filepath2, format = 'stylish') => {
+    const diff = genDiff(filepath1, filepath2, format);
     console.log(diff);
   })
   .parse(process.argv);
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,12 @@
 import parser from './parsers';
 import diff from './difference';
-import stylish from './stylish';
+import lineBuildingFormat from './formater';
 
-const genDiff = (pathToFirstFile, pathToSecondFile) => {
+const genDiff = (pathToFirstFile, pathToSecondFile, format) => {
   const firstFile = parser(pathToFirstFile);
   const secondFile = parser(pathToSecondFile);
   const difference = diff(firstFile, secondFile);
-  const toString = stylish(difference);
+  const toString = lineBuildingFormat(format, difference);
   return toString;
 };
 export default genDiff;
