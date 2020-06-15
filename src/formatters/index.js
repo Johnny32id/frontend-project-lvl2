f901e92514e9e74dad6848b1e6690a69d86dd10a
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-06-15 20:29:18 +0300

    add json format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,15 @@
 import stylish from './stylish';
 import plain from './plain';
+import json from './json';
 
 const lineBuildingFormat = (format, differrence) => {
   let formater;
   switch (format) {
     case ('plain'):
       formater = plain;
+      break;
+    case ('json'):
+      formater = json;
       break;
     default:
       formater = stylish;
