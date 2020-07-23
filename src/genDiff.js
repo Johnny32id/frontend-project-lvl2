00062f492b65e64c1af5079c021d81b70d6ee2a9
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-07-23 20:24:33 +0300

    rename format and restructuring genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,17 @@
-import parser from './parsers';
+import fs from 'fs';
+import path from 'path';
+import parse from './parsers';
 import diff from './difference';
-import lineBuildingFormat from './formatters/index';
+import format from './formatters/index';
 
-const genDiff = (pathToFirstFile, pathToSecondFile, format) => {
-  const firstFile = parser(pathToFirstFile);
-  const secondFile = parser(pathToSecondFile);
-  const difference = diff(firstFile, secondFile);
-  const toString = lineBuildingFormat(format, difference);
+const genDiff = (pathToFirstFile, pathToSecondFile, desiredFormat) => {
+  const extension = path.extname(pathToFirstFile);
+  const firstFileData = fs.readFileSync(pathToFirstFile, 'utf-8');
+  const secondFileData = fs.readFileSync(pathToSecondFile, 'utf-8');
+  const parsedFirstFile = parse(firstFileData, extension);
+  const parsedSecondFile = parse(secondFileData, extension);
+  const difference = diff(parsedFirstFile, parsedSecondFile);
+  const toString = format(desiredFormat, difference);
   return toString;
 };
 export default genDiff;
