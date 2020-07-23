8be72a3ed949874df8910ba2c83469521ce66d4d
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-07-23 20:22:12 +0300

    remove checkValue

--- src/difference.js
+++ src/difference.js
@@ -1,18 +1,3 @@
-const checkValue = (item) => {
-  if (item === true || item === false) {
-    return item;
-  }
-  if (typeof item === 'object') {
-    const newItem = item;
-    const key = Object.keys(newItem);
-    newItem[key] = checkValue(newItem[key]);
-    return newItem;
-  }
-  if (Number.isFinite(Number(item))) {
-    return Number(item);
-  }
-  return item;
-};
 const difference = (firstFile, secondFile) => {
   const allKeys = [...Object.keys(firstFile), ...Object.keys(secondFile)];
   const uniqueKeys = allKeys.filter((item, index) => allKeys.indexOf(item) === index);
@@ -24,18 +9,18 @@ const difference = (firstFile, secondFile) => {
     } else {
       if (valueBefore !== undefined && valueAfter !== undefined) {
         if (valueBefore === valueAfter) {
-          acc.push({ key: name, type: 'not changed', value: checkValue(valueBefore) });
+          acc.push({ key: name, type: 'not changed', value: valueBefore });
         } else {
           acc.push({
-            key: name, type: 'changed', from: checkValue(valueBefore), to: checkValue(valueAfter),
+            key: name, type: 'changed', from: valueBefore, to: valueAfter,
           });
         }
       }
       if (valueBefore !== undefined && valueAfter === undefined) {
-        acc.push({ key: name, type: 'deleted', value: checkValue(valueBefore) });
+        acc.push({ key: name, type: 'deleted', value: valueBefore });
       }
       if (valueBefore === undefined && valueAfter !== undefined) {
-        acc.push({ key: name, type: 'added', value: checkValue(valueAfter) });
+        acc.push({ key: name, type: 'added', value: valueAfter });
       }
     }
     return acc;
