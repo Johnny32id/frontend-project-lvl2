53d6ae3157fb28b122bfc4bad8c61faa43288f1b
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-07-23 20:20:46 +0300

    add tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,32 +1,41 @@
 import path from 'path';
 import fs from 'fs';
-import genDiff from '../src/genDiff';
+import genDiff from '../index';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
 const read = (pathToFile) => fs.readFileSync(getFixturePath(pathToFile), 'utf-8');
-const flatResult = read('flatResult.txt');
-const stylishResult = read('stylishResult.txt');
-const plainResult = read('plainResult.txt');
-const jsonResult = read('jsonResult.json');
-test('stylishJSON', () => {
-  expect(genDiff(getFixturePath('flatBefore.json'), getFixturePath('flatAfter.json'))).toEqual(flatResult);
-  expect(genDiff(getFixturePath('nestedBefore.json'), getFixturePath('nestedAfter.json'))).toEqual(stylishResult);
+let stylishResult;
+let plainResult;
+let jsonResult;
+let beforeJSON;
+let beforeYML;
+let beforeINI;
+let afterJSON;
+let afterYML;
+let afterINI;
+beforeAll(() => {
+  stylishResult = read('stylishResult.txt');
+  plainResult = read('plainResult.txt');
+  jsonResult = read('jsonResult.json');
+  beforeJSON = getFixturePath('before.json');
+  beforeYML = getFixturePath('before.yml');
+  beforeINI = getFixturePath('before.ini');
+  afterJSON = getFixturePath('after.json');
+  afterYML = getFixturePath('after.yml');
+  afterINI = getFixturePath('after.ini');
 });
-test('stylishYML', () => {
-  expect(genDiff(getFixturePath('flatBefore.yml'), getFixturePath('flatAfter.yml'))).toEqual(flatResult);
-  expect(genDiff(getFixturePath('nestedBefore.yml'), getFixturePath('nestedAfter.yml'))).toEqual(stylishResult);
-});
-test('stylishINI', () => {
-  expect(genDiff(getFixturePath('flatBefore.ini'), getFixturePath('flatAfter.ini'))).toEqual(flatResult);
-  expect(genDiff(getFixturePath('nestedBefore.ini'), getFixturePath('nestedAfter.ini'))).toEqual(stylishResult);
+test('stylishFormat', () => {
+  expect(genDiff(beforeJSON, afterJSON)).toEqual(stylishResult);
+  expect(genDiff(beforeYML, afterYML)).toEqual(stylishResult);
+  expect(genDiff(beforeINI, afterINI)).toEqual(stylishResult);
 });
 test('plainFormat', () => {
-  expect(genDiff(getFixturePath('nestedBefore.json'), getFixturePath('nestedAfter.json'), 'plain')).toEqual(plainResult);
-  expect(genDiff(getFixturePath('nestedBefore.yml'), getFixturePath('nestedAfter.yml'), 'plain')).toEqual(plainResult);
-  expect(genDiff(getFixturePath('nestedBefore.ini'), getFixturePath('nestedAfter.ini'), 'plain')).toEqual(plainResult);
+  expect(genDiff(beforeJSON, afterJSON, 'plain')).toEqual(plainResult);
+  expect(genDiff(beforeYML, afterYML, 'plain')).toEqual(plainResult);
+  expect(genDiff(beforeINI, afterINI, 'plain')).toEqual(plainResult);
 });
 test('JSONFormat', () => {
-  expect(genDiff(getFixturePath('nestedBefore.json'), getFixturePath('nestedAfter.json'), 'json')).toEqual(jsonResult);
-  expect(genDiff(getFixturePath('nestedBefore.yml'), getFixturePath('nestedAfter.yml'), 'json')).toEqual(jsonResult);
-  expect(genDiff(getFixturePath('nestedBefore.ini'), getFixturePath('nestedAfter.ini'), 'json')).toEqual(jsonResult);
+  expect(genDiff(beforeJSON, afterJSON, 'json')).toEqual(jsonResult);
+  expect(genDiff(beforeYML, afterYML, 'json')).toEqual(jsonResult);
+  expect(genDiff(beforeINI, afterINI, 'json')).toEqual(jsonResult);
 });
