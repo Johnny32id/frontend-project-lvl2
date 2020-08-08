1bfd54bc3bd0de37942d438cdec9f59a142b5783
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-08-08 16:47:03 +0300

    fix some

--- src/difference.js
+++ src/difference.js
@@ -1,35 +1,26 @@
 import _ from 'lodash';
 
-const difference = (firstFile, secondFile) => {
-  const keysFromFirstFile = Object.keys(firstFile);
-  const keysFromSecondFile = Object.keys(secondFile);
-  const allKeys = keysFromFirstFile.concat(keysFromSecondFile);
-  const uniqueKeys = allKeys.filter((item, index) => allKeys.indexOf(item) === index);
-  const result = uniqueKeys.reduce((acc, name) => {
-    const keyName = name;
-    const valueBefore = firstFile[name];
-    const valueAfter = secondFile[name];
-    if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
-      acc.push({ key: keyName, type: 'not changed', children: difference(valueBefore, valueAfter) });
-    } else {
-      if (keysFromFirstFile.includes(keyName) && keysFromSecondFile.includes(keyName)) {
-        if (valueBefore === valueAfter) {
-          acc.push({ key: keyName, type: 'not changed', value: valueBefore });
-        } else {
-          acc.push({
-            key: keyName, type: 'changed', from: valueBefore, to: valueAfter,
-          });
-        }
+const difference = (firstData, secondData) => {
+  const keys = Object.keys({ ...firstData, ...secondData });
+  const result = keys.map((key) => {
+    const valueBefore = firstData[key];
+    const valueAfter = secondData[key];
+    if (_.has(firstData, key) && _.has(secondData, key)) {
+      if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
+        return { key, type: 'unchanged', children: difference(valueBefore, valueAfter) };
       }
-      if (!keysFromSecondFile.includes(keyName)) {
-        acc.push({ key: keyName, type: 'deleted', value: valueBefore });
+      if (valueBefore === valueAfter) {
+        return { key, type: 'unchanged', value: valueBefore };
       }
-      if (!keysFromFirstFile.includes(keyName)) {
-        acc.push({ key: keyName, type: 'added', value: valueAfter });
-      }
+      return {
+        key, type: 'changed', from: valueBefore, to: valueAfter,
+      };
     }
-    return acc;
-  }, []);
+    if (!_.has(secondData, key)) {
+      return { key, type: 'deleted', value: valueBefore };
+    }
+    return { key, type: 'added', value: valueAfter };
+  });
   return result;
 };
 export default difference;
