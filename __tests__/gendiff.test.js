18904d5fbf84a75b7dfcdff918cdd14842c44191
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-08-08 16:43:13 +0300

    change tests to test.each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,38 +4,10 @@ import genDiff from '../index';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
 const read = (pathToFile) => fs.readFileSync(getFixturePath(pathToFile), 'utf-8');
-let stylishResult;
-let plainResult;
-let jsonResult;
-let beforeJSON;
-let beforeYML;
-let beforeINI;
-let afterJSON;
-let afterYML;
-let afterINI;
-beforeAll(() => {
-  stylishResult = read('stylishResult.txt');
-  plainResult = read('plainResult.txt');
-  jsonResult = read('jsonResult.json');
-  beforeJSON = getFixturePath('before.json');
-  beforeYML = getFixturePath('before.yml');
-  beforeINI = getFixturePath('before.ini');
-  afterJSON = getFixturePath('after.json');
-  afterYML = getFixturePath('after.yml');
-  afterINI = getFixturePath('after.ini');
-});
-test('stylishFormat', () => {
-  expect(genDiff(beforeJSON, afterJSON)).toEqual(stylishResult);
-  expect(genDiff(beforeYML, afterYML)).toEqual(stylishResult);
-  expect(genDiff(beforeINI, afterINI)).toEqual(stylishResult);
-});
-test('plainFormat', () => {
-  expect(genDiff(beforeJSON, afterJSON, 'plain')).toEqual(plainResult);
-  expect(genDiff(beforeYML, afterYML, 'plain')).toEqual(plainResult);
-  expect(genDiff(beforeINI, afterINI, 'plain')).toEqual(plainResult);
-});
-test('JSONFormat', () => {
-  expect(genDiff(beforeJSON, afterJSON, 'json')).toEqual(jsonResult);
-  expect(genDiff(beforeYML, afterYML, 'json')).toEqual(jsonResult);
-  expect(genDiff(beforeINI, afterINI, 'json')).toEqual(jsonResult);
+test.each([
+  ['before.json', 'after.json', 'stylish', 'stylishResult.txt'],
+  ['before.yml', 'after.yml', 'plain', 'plainResult.txt'],
+  ['before.ini', 'after.ini', 'json', 'jsonResult.json'],
+])('genDiff', (before, after, format, result) => {
+  expect(genDiff(getFixturePath(before), getFixturePath(after), format)).toEqual(read(result));
 });
