6f14112bbc8dea406b65a1aa1ac7185e58918176
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-08-11 19:18:06 +0300

    changed to simple checks

--- src/difference.js
+++ src/difference.js
@@ -1,25 +1,25 @@
 import _ from 'lodash';
 
 const difference = (firstData, secondData) => {
-  const keys = Object.keys({ ...firstData, ...secondData });
+  const keys = _.union(Object.keys(firstData), Object.keys(secondData));
   const result = keys.map((key) => {
     const valueBefore = firstData[key];
     const valueAfter = secondData[key];
-    if (_.has(firstData, key) && _.has(secondData, key)) {
-      if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
-        return { key, type: 'unchanged', children: difference(valueBefore, valueAfter) };
-      }
-      if (valueBefore === valueAfter) {
-        return { key, type: 'unchanged', value: valueBefore };
-      }
-      return {
-        key, type: 'changed', from: valueBefore, to: valueAfter,
-      };
+    if (!_.has(firstData, key)) {
+      return { key, type: 'added', value: valueAfter };
     }
     if (!_.has(secondData, key)) {
       return { key, type: 'deleted', value: valueBefore };
     }
-    return { key, type: 'added', value: valueAfter };
+    if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
+      return { key, type: 'unchanged', children: difference(valueBefore, valueAfter) };
+    }
+    if (valueBefore === valueAfter) {
+      return { key, type: 'unchanged', value: valueBefore };
+    }
+    return {
+      key, type: 'changed', from: valueBefore, to: valueAfter,
+    };
   });
   return result;
 };
