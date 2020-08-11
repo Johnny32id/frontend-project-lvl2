8c981f21c232e5eab94620a8f84c9f0da87672fc
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2020-08-11 19:18:24 +0300

    rename functions

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,34 +1,36 @@
 import _ from 'lodash';
 
-const currectedValue = (value) => {
+const convertingValue = (value) => {
   if (typeof value === 'object') {
     return '[complex value]';
   }
   return `'${value}'`;
 };
-const plain = (difference) => {
-  const lineBuilding = (change, node = '') => {
+const buildingPlainFormat = (difference) => {
+  const stringConstruction = (change, node = '') => {
     const build = change.map((item) => {
       const {
         type, key, value, from, to, children,
       } = item;
-      if (_.has(item, 'children')) {
-        return lineBuilding(children, `${node}${key}.`);
+      if (_.isArray(children)) {
+        return stringConstruction(children, `${node}${key}.`);
       }
       switch (type) {
         case 'deleted':
           return `Property '${node}${key}' was deleted`;
         case 'added':
-          return `Property '${node}${key}' was added with value: ${currectedValue(value)}`;
+          return `Property '${node}${key}' was added with value: ${convertingValue(value)}`;
         case 'changed':
-          return `Property '${node}${key}' was changed from ${currectedValue(from)} to ${currectedValue(to)}`;
-        default:
+          return `Property '${node}${key}' was changed from ${convertingValue(from)} to ${convertingValue(to)}`;
+        case 'unchanged':
           return '';
+        default:
+          throw new Error(`Unsuitable type ${type}`);
       }
     });
     return build;
   };
-  const result = lineBuilding(difference).flat(Infinity).filter((el) => el).join('\n');
+  const result = stringConstruction(difference).flat(Infinity).filter((el) => el).join('\n');
   return result;
 };
-export default plain;
+export default buildingPlainFormat;
